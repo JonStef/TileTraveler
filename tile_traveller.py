6b34a74202ed3af7686257b001d5a2a607ae6ff9
Jón Stefánsson <jon18@ru.is>
2019-09-17 10:18:36 +0000

    Algorithm

---
+++ tile_traveller.py
@@ -0,0 +1,12 @@
+
+    #assign startpostion
+    #print possible moves
+    #Get user choise to move
+    #while not end_point
+        #Get user choise to move
+        #Check if choise == end_point
+        #if not - check if move possible
+        #if possible - move
+        #if not - print (Not a valid direction!)
+    #print victory
+
