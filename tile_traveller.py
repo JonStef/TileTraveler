dad77356f79cac5fdc6187e87a447a5c3d895924
Sigthorandri <sigthorandrii@gmail.com>
2019-10-08 09:24:30 +0000

    Pushing Hrafns solution

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,27 +1,102 @@
 
-    #assign startpostion
-def assign_start():
-    ''' assigning the start position '''
-    start_pos = 1
-    return start_pos
+# Constants
+NORTH = 'n'
+EAST = 'e'
+SOUTH = 's'
+WEST = 'w'
 
-def user_input():
-    ''' ask the user for input '''
-    user_input = chr(ord(input("Input your choice: ")))
-    return user_input
+def move(direction, col, row):
+    ''' Returns updated col, row given the direction '''
+    if direction == NORTH:
+        row += 1
+    elif direction == SOUTH:
+        row -= 1
+    elif direction == EAST:
+        col += 1
+    elif direction == WEST:
+        col -= 1
+    return(col, row)    
 
-def position():
-    print("You can travel: ")
-    #if user_input = 4 
-    #position 2 n s 
+def is_victory(col, row):
+    ''' Return true is player is in the victory cell '''
+    return col == 3 and row == 1 # (3,1)
 
-    #print possible moves!
-    #Get user choise to move
-    #while not end_point
-        #Get user choise to move
-        #Check if choise == end_point
-        #if not - check if move possible
-        #if possible - move
-        #if not - print (Not a valid direction!)
-    #print victory!
+def print_directions(directions_str):
+    print("You can travel: ", end='')
+    first = True
+    for ch in directions_str:
+        if not first:
+            print(" or ", end='')
+        if ch == NORTH:
+            print("(N)orth", end='')
+        elif ch == EAST:
+            print("(E)ast", end='')
+        elif ch == SOUTH:
+            print("(S)outh", end='')
+        elif ch == WEST:
+            print("(W)est", end='')
+        first = False
+    print(".")
 
+def total_check():
+    total = 0
+    total += 1
+    return total
+    
+def lever_check():
+    question = input("Pull a lever (y/n): ").lower()
+    coin = 0
+    if question == 'y':
+        coin = coin + 1
+        print("You recieved {} coin".format(coin),"and your total is now", total_check())
+
+def find_directions(col, row):
+    ''' Returns valid directions as a string given the supplied location '''
+    if col == 1 and row == 1:   # (1,1)
+        valid_directions = NORTH
+    elif col == 1 and row == 2: # (1,2)
+        valid_directions = NORTH+EAST+SOUTH
+        lever_check()
+    elif col == 1 and row == 3: # (1,3)
+        valid_directions = EAST+SOUTH
+    elif col == 2 and row == 1: # (2,1)
+        valid_directions = NORTH
+    elif col == 2 and row == 2: # (2,2)
+        valid_directions = SOUTH+WEST
+    elif col == 2 and row == 3: # (2,3)
+        valid_directions = EAST+WEST
+    elif col == 3 and row == 2: # (3,2)
+        valid_directions = NORTH+SOUTH
+    elif col == 3 and row == 3: # (3,3)
+        valid_directions = SOUTH+WEST
+    return valid_directions
+
+def play_one_move(col, row, valid_directions):
+    ''' Plays one move of the game
+        Return if victory has been obtained and updated col,row '''
+    victory = False
+    direction = input("Direction: ")
+    direction = direction.lower()
+    
+    if not direction in valid_directions:
+        print("Not a valid direction!")
+    else:
+        col, row = move(direction, col, row)
+        victory = is_victory(col, row)
+    return victory, col, row
+
+# The main program starts here
+victory = False
+row = 1
+col = 1
+
+valid_directions = NORTH
+print_directions(valid_directions)
+
+while not victory:
+    victory, col, row = play_one_move(col, row, valid_directions)
+    if victory:
+        print("Victory!")
+    else:
+        valid_directions = find_directions(col, row)
+        print_directions(valid_directions)
